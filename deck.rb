0080b52850c60f6e8c25618c937389d9a6b81d84
v3rba <verbush@gmail.com>
2018-10-25 09:06:48 +0300

    Initialize array of cards with freeze method

---
+++ deck.rb
@@ -0,0 +1,17 @@
+class Deck
+
+  SUITS = %w[♠️♣️♥️♦️].freeze
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  attr_reader :cards
+
+  def initialize
+  end
+
+  def shuffle
+  end
+
+  def deal_card
+  end
+
+end
