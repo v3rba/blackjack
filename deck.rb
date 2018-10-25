eab0599d057ee42d9e57fde09a410921fe299cb4
v3rba <verbush@gmail.com>
2018-10-25 12:19:06 +0300

    Add initialize method for new Deck class

--- deck.rb
+++ deck.rb
@@ -1,14 +1,12 @@
 class Deck
 
-  SUITS = %w[♠️♣️♥️♦️].freeze
+  SUITS = %w[♠️ ♣️ ♥️ ♦️].freeze
   RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
 
   attr_reader :cards
 
   def initialize
-  end
-
-  def shuffle
+    @cards = Deck.build_deck
   end
 
   def build_deck
