8fda430234e9c7f6a13353fa6e3ff1b3057164b6
v3rba <verbush@gmail.com>
2018-10-29 22:16:03 +0200

    Changed take_a_card method

--- deck.rb
+++ deck.rb
@@ -2,13 +2,11 @@ class Deck
   SUITS = %w[♠️ ♣️ ♥️ ♦️].freeze
   RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
 
-  attr_reader :cards
-
   def initialize
     @cards = build_deck
   end
 
-  def build_deck
+  def self.build_deck
     @cards = []
     SUITS.each do |suit|
       RANKS.each do |rank|
@@ -18,7 +16,7 @@ class Deck
     @cards.shuffle!
   end
 
-  def deal_cards(num = 1)
-    num == 1 ? cards.pop : cards.pop(num)
+  def take_a_card
+    @cards.pop
   end
 end
