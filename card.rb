1397cbbdcf3654f713b55f6444d2efba68d9be4f
v3rba <verbush@gmail.com>
2018-10-30 08:58:56 +0200

    Add calculation points method

--- card.rb
+++ card.rb
@@ -1,9 +1,12 @@
 class Card
-  attr_reader :rank, :suit
+  attr_reader :rank, :suit, :points
+
+  HIDDEN = '**'.freeze
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+    @points = points
   end
 
   def face_card?
@@ -12,5 +15,19 @@ class Card
 
   def ace_card?
     %w[A].include?(rank)
+  end
+
+  def points
+    return 11 if ace_card?
+    return 10 if face_card?
+    @rank
+  end
+
+  def to_s
+    "#{@rank}#{@suit}"
+  end
+
+  def hidden
+    HIDDEN
   end
 end
