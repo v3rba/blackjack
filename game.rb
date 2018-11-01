db20d98a9b6d9380626df466a52ac37b068e1600
v3rba <verbush@gmail.com>
2018-11-01 13:12:34 +0200

    Add has_enough_money? to break the game if player run out of money

--- game.rb
+++ game.rb
@@ -1,14 +1,27 @@
-require './player.rb'
-require './card.rb'
-require './deck.rb'
+require_relative 'player'
+require_relative 'card'
+require_relative 'deck'
+require_relative 'interface'
 
 class Game
+  include Interface
   attr_accessor :user, :dealer, :deck, :bank
 
   def initialize(user, dealer = Dealer.new)
     @user = user
     @dealer = dealer
     @bank = 0
+  end
+
+  def play_round
+    while has_enough_money?
+      new_round
+      until round_end?
+        user_turn
+        dealer_turn
+      end
+      open_cards
+    end
   end
 
   def new_round
@@ -21,15 +34,6 @@ class Game
       2.times { take_card(player) }
     end
     @bank = 20
-  end
-
-  def play_round
-    new_round
-    until round_end?
-      user_turn
-      dealer_turn
-    end
-    open_cards
   end
 
   def user_turn
@@ -50,10 +54,6 @@ class Game
     player.cards << deck.pull_out
   end
 
-  def skip_turn(player)
-    puts "#{player.name} pass the turn!"
-  end
-
   def open_cards
     [user, dealer].each { |p| show_cards(p, true) }
     winner = check_winner
@@ -71,51 +71,13 @@ class Game
     end
   end
 
-  def user_select
-    puts 'Select action:'
-    puts '(T)ake card, (S)kip turn, (O)pen cards'
-    choise = gets.chomp.downcase
-    case choise
-    when 't'
-      take_card(user)
-    when 's'
-      skip_turn(user)
-    when 'o'
-      open_cards
+  def has_enough_money?
+    if user.has_money? && dealer.has_money?
+      true
     else
-      puts 'Type error!'
-    end
-  end
-
-  def show_cards(player, visible = true)
-    puts "#{player.name}'s cards:"
-    puts '|*|' * player.cards.size if visible == false
-    puts player.cards if visible == true
-    puts '-' * 10
-  end
-
-  def start_new_round
-    puts 'Do you want play again? (Y, N)'
-    choise = gets.chomp.downcase
-    if choise == 'y'
-      play_round
-    else
+      game_over
       exit
     end
-  end
-
-  def print_info(player, attr)
-    puts "#{player.name}'s #{attr.to_s.capitalize}: #{player.send(attr)}"
-  end
-
-  def print_round_end(winner)
-    if winner == 'Draw!'
-      puts winner
-    else
-      puts "#{winner.name} WIN!"
-    end
-    [user, dealer].each { |p| print_info(p, :points) }
-    [user, dealer].each { |p| print_info(p, :money) }
   end
 
   def round_end?
@@ -125,16 +87,6 @@ class Game
       true
     else
       false
-    end
-  end
-
-  def check_winner
-    if (user.points > dealer.points) && user.points <= 21
-      user
-    elsif user.points == dealer.points
-      'Draw!'
-    else
-      dealer
     end
   end
 end
