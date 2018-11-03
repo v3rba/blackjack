0de4c9090a3a2d585512a7cb0921fd4bc8a49c4c
v3rba <verbush@gmail.com>
2018-11-03 11:09:23 +0200

    Add round_end and check_winner validation

--- checking.rb
+++ checking.rb
@@ -1,2 +1,21 @@
 module Checking
+    def check_winner
+    if (user.points > dealer.points) && user.points <= 21
+      user
+    elsif user.points == dealer.points
+      'Draw!'
+    else
+      dealer
+    end
+  end
+
+  def round_end?
+    if user.limit_cards? && dealer.limit_cards?
+      true
+    elsif user.limit_cards? && !dealer.take_card?
+      true
+    else
+      false
+    end
+  end
 end
