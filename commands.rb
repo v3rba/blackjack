5605c37ce1c3bc0b7c27d7ce5aebd2e24e25de21
v3rba <verbush@gmail.com>
2018-11-10 16:49:53 +0200

    Commit

--- commands.rb
+++ commands.rb
@@ -45,4 +45,26 @@ module Commands
       puts "#{user.name}, Congratulations! Dealer is out of money. You have won. Good bye"
     end
   end
+
+  def check_winner
+    if (user.points > dealer.points) && user.points <= 21
+      user
+    elsif (user.points < dealer.points) && dealer.points > 21
+      user
+    elsif user.points == dealer.points
+      'Draw!'
+    else
+      dealer
+    end
+  end
+
+  def round_end?
+    if user.limit_cards? && dealer.limit_cards?
+      true
+    elsif user.limit_cards? && !dealer.take_card?
+      true
+    else
+      false
+    end
+  end
 end
