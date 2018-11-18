fcae84964a8245aa98aa3ce78c1e1134c6bd718d
v3rba <verbush@gmail.com>
2018-11-18 17:10:24 +0200

    Final fix

--- main.rb
+++ main.rb
@@ -1,19 +1,12 @@
-require_relative 'player'
-require_relative 'card'
 require_relative 'deck'
-require_relative 'commands'
+require_relative 'player'
 require_relative 'user'
-require_relative 'game'
 require_relative 'dealer'
+require_relative 'bank'
+require_relative 'my_game'
+require_relative 'interface'
 
-@deck = Deck.new
-@deck.shuffle!
-puts 'Welcome to Blackjack!'
-puts "I'm going to be the dealer, and my name is Joe Rogan."
-puts "If you don't mind me asking, what's your name?"
-username = gets.chomp.capitalize
-puts "Nice to meet you, #{username}! Let's get started."
-user = User.new(username)
-dealer = Dealer.new
-game = Game.new(user, dealer)
-game.start_round
+interface = Interface.new
+
+game = MyGame.new(interface)
+game.run
