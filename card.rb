e573544d5d38b17bf62a91caa6abe9c4e50d4f0b
v3rba <verbush@gmail.com>
2018-11-18 17:04:44 +0200

    Refactored card class

--- card.rb
+++ card.rb
@@ -1,26 +1,8 @@
 class Card
-  attr_reader :rank, :suit
-  def initialize(rank, suit)
-    @rank = rank
+  attr_reader :suit, :face
+
+  def initialize(suit, face)
     @suit = suit
-  end
-
-  def get_points
-    ranks = %i[k q j]
-    if ranks.include?(rank)
-      10
-    elsif rank == :a
-      [1, 11]
-    else
-      rank.to_i
-    end
-  end
-
-  def ace?
-    rank == :a
-  end
-
-  def to_s
-    "#{rank}" "#{suit}"
+    @face = face
   end
 end
