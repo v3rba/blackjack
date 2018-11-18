cfa00d4b3d271d138d0be18b7bc5e538c515a686
v3rba <verbush@gmail.com>
2018-11-18 17:09:24 +0200

    Refactored interface class

--- interface.rb
+++ interface.rb
@@ -1,21 +1,68 @@
 class Interface
+  attr_reader :choice
 
-  def start_round
-    loop do
-      puts 'Wanna play? (Y/N)'
-      case gets.chomp
-      when 'y'
-        new_round
-        until round_end? && enough_money?
-          user_turn
-          dealer_turn
-        end
-        open_cards
-      when 'n'
-        puts 'Bye'
-        abort
-      end
+  def victory_report
+    puts 'You win'
+  end
+
+  def defeat_report
+    puts 'You lose'
+  end
+
+  def draw_report
+    puts 'Draw'
+  end
+
+  def message_to_continue
+    puts 'Do you want to continue? y/n'
+    @choice = gets.chomp
+  end
+
+  def selection_error
+    puts 'Choose from the offered options'
+  end
+
+  def user_cards_message(player)
+    puts 'Your cards:'
+    player.cards.each do |card|
+      puts "#{card.face}#{card.suit}"
     end
   end
 
+  def user_points_message(player)
+    puts "Your points: #{player.sum}"
+  end
+
+  def dealer_cards_message(player)
+    puts 'Dealer cards:'
+    player.cards.each do |card|
+      puts "#{card.face}#{card.suit}"
+    end
+  end
+
+  def unknown_dealer_cards
+    puts %(
+    Dealer cards:
+    1.**
+    2.**
+   )
+  end
+
+  def dealer_points_message(player)
+    puts "Dealer points: #{player.sum}"
+  end
+
+  def user_chioce
+    puts %(
+    1. Skip
+    2. Take card
+    3. Open cards
+    )
+    @choice = gets.chomp
+  end
+
+  def enter_name
+    puts 'Enter name: '
+    @name = gets.chomp
+  end
 end
